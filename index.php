7746a4b28c43d18b5314617332151fd04219c559
Dzemi <dzemikq@gmail.com>
2021-08-29 22:52:20 +0200

    Up

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta charset="UTF-8">
+<meta name=:descruption" content="Share your autograph!" />
+<title>Autographs</title>
+</head>
+<body>
+<form action="*">
+<label>Nazwisko</label> <input type="text" name="nazwisko" />
+</form>
+</body>
+</html>
